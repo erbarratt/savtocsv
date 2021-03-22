95c4713c31bcf13f33eed3446b784389a216c349
Elliott Barratt <elbazuk@gmail.com>
2021-03-22 12:06:17 +0000

    Update savtocsvcommon.h

--- savtocsvcommon.h
+++ savtocsvcommon.h
@@ -1,5 +1,9 @@
+#include "common.h"
 
-
+extern bool debug;
+extern bool silent;
+extern bool longCsv;
+extern bool includeRowIndex;
 extern char *sav;
 extern char *csv;
 extern int lineLimit;
@@ -12,11 +16,11 @@ extern char ANSI_COLOR_MAGENTA[];
 extern char ANSI_COLOR_CYAN[];
 extern char ANSI_COLOR_RESET[];
 
-void exitSavtocsv();
 void colorSet(char *col);
 void printOut(char *str, char *bound, char *col);
 void printOutErr(char *str, char *bound);
 extern char *getFileExt(char *filename);
+char* intToStr8(int8_t num);
 char* intToStr32(int num);
 char* intToStr64(int64_t num);
 char* doubleToStr(double num);
