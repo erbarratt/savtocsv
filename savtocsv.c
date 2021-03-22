1b6c98c4a533f60b8017f9ac996aca2f3a600cfa
Elliott Barratt <elbazuk@gmail.com>
2021-03-22 14:24:18 +0000

    Update savtocsv.c

--- savtocsv.c
+++ savtocsv.c
@@ -39,7 +39,7 @@ void parseOpts(int argc, char *argv[]){
 						printf(ANSI_COLOR_RESET);
 						printf("version ");
 						printf(ANSI_COLOR_YELLOW);
-						printf("version 1.5 ");
+						printf("version 1.5.1 ");
 						printf(ANSI_COLOR_RESET);
 						printf("2021-03-22\n");
 						exit(0);
