125c7605328368efd086fbdd12862f70aead8553
Elliott Barratt <elbazuk@gmail.com>
2021-03-22 12:05:22 +0000

    headers

--- savtocsvlib.c
+++ savtocsvlib.c
@@ -1,23 +1,4 @@
-#include "common.h"
 #include "savtocsvlib.h"
-#include "savtocsvcommon.h"
-
-
-#define RECORD_TYPE_VARIABLE 2
-#define RECORD_TYPE_VALUE_LABELS 3
-#define RECORD_TYPE_VALUE_LABELS_INDEX 4
-#define RECORD_TYPE_DOCUMENTS 6
-#define RECORD_TYPE_ADDITIONAL 7
-#define RECORD_TYPE_FINAL 999
-
-#define COMPRESS_SKIP_CODE 0
-#define COMPRESS_END_OF_FILE 252
-#define COMPRESS_NOT_COMPRESSED 253
-#define COMPRESS_ALL_BLANKS 254
-#define COMPRESS_MISSING_VALUE 255
-
-//Number of bytes really stored in each segment of a very long string variable.
-#define REAL_VLS_CHUNK 255
 
 /** @var bool bigEndian flag to see if file was oringinally stored on Big Endian OS... */
 	bool bigEndian = false;
@@ -54,15 +35,6 @@
 	
 /** @var int numberOfVariables Num vars in sav file */
 	int numberOfVariables = 0;
-
-/** @var struct Variable Variable structure definition */
-	typedef struct Variable{
-		int type;
-		int measure;
-		int cols;
-		int alignment;
-		struct Variable * next;
-	} variable_t;
 
 /** @var variable_t* variablesList Linked list of Variable structures */
 	variable_t * variablesList = NULL;
